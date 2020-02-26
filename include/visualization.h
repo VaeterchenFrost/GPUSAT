81b00b42ad60c4e06f44285ecd809ab4ef85a41e
Martin <martin.roebke@web.de>
2020-02-26 11:44:32 +0100

    Adding TABLELINES and Grid in visu

--- include/visualization.h
+++ include/visualization.h
@@ -10,6 +10,44 @@
 #include <json/writer.h>
 
 namespace gpusat {
+/**
+ * Class for storing 2dim 'unsigned int' values
+ * using unique_ptr and a 1dim memory layout.
+ * See https://stackoverflow.com/a/32279494
+ */
+class Grid {
+    size_t _rows;
+    size_t _columns;
+    std::unique_ptr<unsigned int[]> data;
+
+  public:
+    Grid(size_t rows, size_t columns)
+        : _rows{rows},
+          _columns{columns},
+          data{std::make_unique<unsigned int[]>(rows * columns)} {}
+
+    size_t rows() const { return _rows; }
+
+    size_t columns() const { return _columns; }
+
+    unsigned int *operator[](size_t row) { return row * _columns + data.get(); }
+
+    unsigned int &operator()(size_t row, size_t column) {
+        return data[row * _columns + column];
+    }
+};
+
+/**
+ * Solutions in Timeline. Example:
+ * [["id", "v2", "v4", "n Sol"],
+ *  [0, 0, 0, 1], [1, 1, 0, 2], [2, 0, 1, 2],
+ *  [3, 1, 1, 3]]
+ */
+struct TABLELINES {
+    unsigned int columns;
+    std::string *headline;
+    Grid solutions;
+};
 
 class Visualization {
 
@@ -42,7 +80,7 @@ class Visualization {
     void visuTD(treedecType *treeDec);
     Json::StreamWriterBuilder *getWriterBuilder();
     void writeJsonToStdout(Json::StreamWriter::Factory const &factory, Json::Value const &value);
-    void visuTimelineMain();
+    void visuMainTimeline();
 
   private:
     bool outputEnabled = false;
