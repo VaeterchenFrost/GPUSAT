ce26b36d1d8fc71ca32747534b39bbcfa565819b
Martin <martin.roebke@web.de>
2020-05-02 15:10:46 +0200

    Start API 1.1 clear old comments, insert some spaces, update visuSatForm

--- src/visualization.cpp
+++ src/visualization.cpp
@@ -5,76 +5,17 @@
 #include <sstream>
 #include <types.h>
 #include <visualization.h>
+
 #define LOGGER2(x)
 #define LOGGER(x) (std::cout << "LOGGING: " << x << std::endl)
 
-/*
-TD GRAPH:
-    s.node(bagpre % 4, bagNode(bagpre % 4, ["[2 3 8]", "Test2"]))
-    s.node(bagpre % 3, bagNode(bagpre % 3, "[2 4 8]"))
-    # s.node('join1', bagNode("Join", "2~3"))
-    s.node(bagpre % 2, bagNode(bagpre % 2, "[1 2 5]"))
-    s.node(bagpre % 1, bagNode(bagpre % 1, "[1 2 4 6]"))
-    s.node(bagpre % 0, bagNode(bagpre % 0, "[1 4 7]"))
-
-    s.edges(
-        [(bagpre % 4, bagpre % 3), (bagpre % 2, bagpre % 1),
-         (bagpre % 3, bagpre % 1), (bagpre % 1, bagpre % 0)])
-
-TD-TIMELINE:
-TIMELINE = [(0,), (1,),
-                (2, ([['id', 'v1', 'v2', 'n Sol'], [0, 0, 0, 0], [1, 1, 0, 1],
-                      [2, 0, 1, 1], [3, 1, 1, 2]], 'sol bag 2', 'sum: 4',
-True)), (3,), (4, ([["id", "v2", "v8", "n Sol"], [0, 0, 0, 1], [1, 1, 0, 2], [2,
-0, 1, 1], [3, 1, 1, 1]], "sol bag 4", "sum: 5", True)), (3, ([["id", "v2", "v4",
-"n Sol"], [0, 0, 0, 1], [1, 1, 0, 2], [2, 0, 1, 2], [3, 1, 1, 3]], "sol bag 3",
-"sum: 8", True)),
-                ((2, 3), ([["id", "v1", "v2", "v4", "n Sol"],
-                           [0, 0, 0, 0, 0],
-                           [1, 1, 0, 0, 1],
-                           [2, 0, 1, 0, 2],
-                           [3, 1, 1, 0, 4],
-                           [4, 0, 0, 1, 0],
-                           [5, 1, 0, 1, 2],
-                           [6, 0, 1, 1, 3],
-                           [7, 1, 1, 1, 6]],
-                          "sol Join 2~3", "sum: 18", True)),
-                (1, ([["id", "v1", "v4", "n Sol"],
-                      [0, 0, 0, 2], [1, 1, 0, 9], [2, 0, 1, 3],
-                      [3, 1, 1, 6]], "sol bag 1", "sum: 20", True)),
-                (0, ([["id", "v1", "v4", "v7", "n Sol"],
-                      [0, 0, 0, 0, 2],
-                      [1, 1, 0, 0, 0],
-                      [2, 0, 1, 0, 0],
-                      [3, 1, 1, 0, 0],
-                      [4, 0, 0, 1, 2],
-                      [5, 1, 0, 1, 9],
-                      [6, 0, 1, 1, 3],
-                      [7, 1, 1, 1, 6]],
-                     "sol bag 0", "sum: 22", True))
-                ]
-
-INCIDENCE:
-EDGELIST = [[1, [1, 4, 6]], [2, [1, -5]], [3, [-1, 7]], [4, [2, 3]], [5, [2,
-5]], [6, [2, -6]], [7, [3, -8]], [8, [4, -8]], [9, [-4, 6]], [10, [-4, 7]]]
-TIMELINE = (None, None,
-                [1, 2, 5],
-                None,
-                [2, 3, 8],
-                [2, 4, 8],
-                [1, 2, 4],  # JOIN
-
-                [1, 2, 4, 6],
-                [1, 4, 7]
-                )
-*/
 namespace gpusat {
 
 void Visualization::writeJsonToStdout(
     Json::StreamWriter::Factory const &factory, Json::Value const &value, Json::OStream *sout) {
     std::unique_ptr<Json::StreamWriter> const writer(factory.newStreamWriter());
     writer->write(value, sout);
-    std::cout << std::endl; // add lf and flush
+    std::cout << std::endl;
 }
 
 void Visualization::writeJsonToStdout(Json::Value const &value, Json::OStream *sout) {
@@ -130,6 +71,7 @@ void Visualization::visuTreeDec(treedecType *treeDec) {
 
     tdGraph[TAG_BAGPRE] = BAGPRE;
     tdGraph[TAG_NUMVARS] = treeDec->numVars;
+
     for (auto bag : treeDec->bags) {
         std::ostringstream variables;
         Json::Value var_list;
@@ -258,15 +200,14 @@ void Visualization::writeJsonFile(bool append) {
         } else {
             LOGGER("tdTimeline not array-type");
         }
-
         writeJsonToStdout(visu_json, &stream);
-
         stream.close();
     } else {
         std::cerr << "Failed to open file : " << visufile << " with " << errno
                   << std::endl;
     }
 }
+
 /**
  * Store a new filename.
  * In case the name has 0 length like "",
@@ -279,8 +220,10 @@ void Visualization::setFile(std::string filename) {
         outputEnabled = false;
     visufile = filename;
 }
+
 BagMatrix::BagMatrix(size_t rows, size_t cols) : mRows(rows), mCols(cols), mData(rows * cols) {
 }
+
 BagMatrix::BagMatrix() : mRows(0), mCols(0), mData(0){};
 
 cl_long &BagMatrix::operator()(size_t i, size_t j) {
@@ -340,7 +283,7 @@ TableLines solJson(bagType node, dataStructure solutionType) {
 /**
  * Save the clauses into the Visualization::clausesJson.
  * Form: 
- *  List[Dict{"id": List[Int]}]
+ *  List[Dict{INT: List[Int]}]
  * Error if:
  *  sat formula is null or empty.
  */
@@ -350,7 +293,7 @@ void Visualization::visuSatForm(satformulaType *sat) {
         return;
     }
 
-    Json::Value resultJsom;
+    Json::Value resultJson;
     Json::Value varsArr;
     Json::Value clauseJ;
     auto clause_counter = 0u;
@@ -362,11 +305,11 @@ void Visualization::visuSatForm(satformulaType *sat) {
         clauseJ["id"] = ++clause_counter;
         clauseJ["list"] = varsArr;
 
-        resultJsom.append(clauseJ);
+        resultJson.append(clauseJ);
         varsArr.clear();
         clauseJ.clear();
     }
 
-    clausesJson = resultJsom;
+    clausesJson = resultJson;
 }
 } // namespace gpusat
